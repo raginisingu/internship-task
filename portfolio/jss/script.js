fd2bf018aa6bfce1b95331d4fa69dc7ef7d55049
raginisingu <168798733+raginisingu@users.noreply.github.com>
2024-07-21 20:34:45 +0530

    Add files via upload

---
+++ portfolio/jss/script.js
@@ -0,0 +1,7 @@
+/*=================================== typing animation ==================================*/
+var typed = new Typed(".typing",{
+    strings:["","student"],
+    typespeed:100,
+    Bachspeed:60,
+    loop:true
+})
